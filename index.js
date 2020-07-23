04437254fcffbfcc094a0e6894599fbd29affed5
Mosiur-Rahman-1 <masiurreheman01@gmail.com>
2020-07-24 03:05:35 +0600

    Bank website complete

--- index.js
+++ index.js
@@ -18,13 +18,39 @@ depositeBtn.addEventListener ("click", function () {
 
     var depositeAmount = document.getElementById ("deposite-amount").value;
     var depositeInputNumber = parseFloat (depositeAmount);
+
     var depositeAmountChange = document.getElementById ("deposite-amount-change").innerText;
     var depositeAmountNumber = parseFloat(depositeAmountChange);
+
     var totalDeposite = depositeInputNumber + depositeAmountNumber;
     document.getElementById ("deposite-amount-change").innerText = totalDeposite;
     document.getElementById ("deposite-amount").value = "";
+
+
     var totalBalance = document.getElementById ("total-balance-change").innerText;
     var totalBalanceNumber = parseFloat(totalBalance);
-    var totalFinalBalance = totalDeposite + totalBalanceNumber;
+    var totalFinalBalance = depositeInputNumber + totalBalanceNumber;
     document.getElementById ("total-balance-change").innerText = totalFinalBalance;
+})
+
+
+// Withdraw button event handlar
+
+var withdrawBtn = document.getElementById ("withdraw-button");
+withdrawBtn.addEventListener ("click", function () {
+
+    var withdrawAmount = document.getElementById ("withdraw-amount").value;
+    var withdrawAmountNumber = parseFloat(withdrawAmount);
+    document.getElementById ("withdraw-amount").value = "";
+    
+    var withdrawAmountChange = document.getElementById ("withdraw-amount-change").innerText;
+    var withdrawAmountChangeNumber = parseFloat (withdrawAmountChange);
+    var totalWithdraw = withdrawAmountChangeNumber + withdrawAmountNumber;
+    document.getElementById ("withdraw-amount-change").innerText = totalWithdraw;
+
+    var totalBalance = document.getElementById ("total-balance-change").innerText;
+    var totalBalanceNumber = parseFloat(totalBalance);
+    var totalFinalBalance = totalBalanceNumber - 1* withdrawAmountNumber;
+    document.getElementById ("total-balance-change").innerText = totalFinalBalance;
+    
 })
